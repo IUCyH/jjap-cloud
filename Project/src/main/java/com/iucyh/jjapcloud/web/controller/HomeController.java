59bad9e5c5a439fe6a61b38a44259b7b83070abc
Jaemin <ghdckdwn8456@gmail.com>
2025-05-17 22:42:58 +0900

    [feat] HomeController 생성

---
+++ Project/src/main/java/com/iucyh/jjapcloud/web/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.iucyh.jjapcloud.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "hello";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "ok";
+    }
+}
