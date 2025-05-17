706c56d98b0cb6776f489bf450a79a4953b26532
Jaemin <ghdckdwn8456@gmail.com>
2025-05-17 22:37:56 +0900

    init

---
+++ Project/src/main/java/com/iucyh/jjapcloud/JjapCloudApplication.java
@@ -0,0 +1,12 @@
+package com.iucyh.jjapcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjapCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjapCloudApplication.class, args);
+	}
+}
---
+++ Project/src/test/java/com/iucyh/jjapcloud/JjapCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iucyh.jjapcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjapCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
