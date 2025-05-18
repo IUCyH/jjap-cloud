1b6d6dc945c4e84bbd37471f99f164085e2eb304
Jaemin <ghdckdwn8456@gmail.com>
2025-05-19 02:34:25 +0900

    [feat] 로깅 인터셉터 구현

---
+++ Project/src/main/java/com/iucyh/jjapcloud/AppConfig.java
@@ -0,0 +1,18 @@
+package com.iucyh.jjapcloud;
+
+import com.iucyh.jjapcloud.web.common.interceptor.LoggingInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggingInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/ico/**", "/css", "/error");
+    }
+}
---
+++ Project/src/main/java/com/iucyh/jjapcloud/web/common/interceptor/LoggingInterceptor.java
@@ -0,0 +1,33 @@
+package com.iucyh.jjapcloud.web.common.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.UUID;
+
+@Slf4j
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String uuid = UUID.randomUUID().toString();
+        MDC.put("traceId", uuid);
+
+        long now = System.currentTimeMillis();
+        request.setAttribute("startTime", now);
+
+        log.info("REQUEST: {}", request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        long startTime = (long)request.getAttribute("startTime");
+        long endTime = System.currentTimeMillis();
+
+        log.info("RESPONSE: {} - {} ms", response.getStatus(), (endTime - startTime));
+    }
+}
