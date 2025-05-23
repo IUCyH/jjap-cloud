7b5d2b7d97ff46e78dd831614eb420032ac7eb9a
Jaemin <ghdckdwn8456@gmail.com>
2025-05-23 23:20:58 +0900

    [feat] 음악 스트리밍 테스트 페이지 작성

--- Project/src/main/java/com/iucyh/jjapcloud/web/controller/MusicViewController.java
+++ Project/src/main/java/com/iucyh/jjapcloud/web/controller/MusicViewController.java
@@ -1,15 +1,31 @@
 package com.iucyh.jjapcloud.web.controller;
 
+import com.iucyh.jjapcloud.web.dto.music.MusicDto;
+import com.iucyh.jjapcloud.web.service.music.MusicService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//@Controller
-//@RequestMapping("/music-view")
-//public class MusicViewController {
+//import java.util.List;
 //
-//    @GetMapping("/upload")
-//    public String showUploadForm() {
-//        return "music-upload";
-//    }
+////@Controller
+////@RequestMapping("/music-view")
+////@RequiredArgsConstructor
+////public class MusicViewController {
+////
+////    private final MusicService musicService;
+////
+////    @GetMapping("/upload")
+////    public String showUploadForm() {
+////        return "music-upload";
+////    }
+////
+////    @GetMapping("/stream")
+////    public String showStreamPage(Model model) {
+////        List<MusicDto> musics = musicService.getMusics(null);
+////        model.addAttribute("musics", musics);
+////        return "music-stream";
+////    }
 //}
