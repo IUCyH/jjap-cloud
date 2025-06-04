0482df72f6d81bfa00567ddd5f84dc5b6071af1f
Jaemin <ghdckdwn8456@gmail.com>
2025-06-05 06:22:38 +0900

    [rafactor] MusicService 인터페이스 삭제 및 구현체 직접 주입

--- Project/src/main/java/com/iucyh/jjapcloud/service/music/MusicService.java
+++ Project/src/main/java/com/iucyh/jjapcloud/service/music/MusicService.java
@@ -2,26 +2,74 @@ package com.iucyh.jjapcloud.service.music;
 
 import com.iucyh.jjapcloud.common.exception.ServiceException;
 import com.iucyh.jjapcloud.common.exception.errorcode.ServiceErrorCode;
-import com.iucyh.jjapcloud.common.wrapper.LimitedInputStream;
+import com.iucyh.jjapcloud.common.util.FileManager;
 import com.iucyh.jjapcloud.domain.music.Music;
 import com.iucyh.jjapcloud.dto.IdDto;
 import com.iucyh.jjapcloud.dto.music.CreateMusicDto;
 import com.iucyh.jjapcloud.dto.music.MusicDto;
-import com.iucyh.jjapcloud.dto.music.RangeDto;
 import com.iucyh.jjapcloud.dto.music.SearchMusicCondition;
-import org.springframework.core.io.InputStreamResource;
+import com.iucyh.jjapcloud.repository.music.MusicQueryRepository;
+import com.iucyh.jjapcloud.repository.music.MusicRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
-public interface MusicService {
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MusicService {
 
-    List<MusicDto> getMusics(Date date);
-    List<MusicDto> searchMusics(SearchMusicCondition condition, Date date);
-    MusicDto getMusicById(int id);
-    IdDto createMusic(CreateMusicDto music);
-    void deleteMusic(int id);
+    private final MusicRepository musicRepository;
+    private final MusicQueryRepository musicQueryRepository;
+    private final FileManager fileManager;
+
+    public List<MusicDto> getMusics(Date date) {
+        List<Music> musics = musicQueryRepository.findMusics(date);
+        return musics
+                .stream()
+                .map(MusicDto::from)
+                .toList();
+    }
+
+    public List<MusicDto> searchMusics(SearchMusicCondition condition, Date date) {
+        List<Music> musics = musicQueryRepository.searchMusics(condition.getField(), condition.getKeyword(), date);
+        return musics
+                .stream()
+                .map(MusicDto::from)
+                .toList();
+    }
+
+    public MusicDto getMusicById(int id) {
+        return musicRepository
+                .findById(id)
+                .map(MusicDto::from)
+                .orElseThrow(() -> new ServiceException(ServiceErrorCode.MUSIC_NOT_FOUND));
+    }
+
+    @Transactional
+    public IdDto createMusic(CreateMusicDto music) {
+        if(!fileManager.isCorrectMimeType(music.getMusicFile(), "audio/mpeg")) {
+            throw new ServiceException(ServiceErrorCode.NOT_VALID_MUSIC_FILE);
+        }
+
+        String storeName = fileManager.uploadFile(music.getMusicFile(), "music");
+        long playTime = fileManager.getPlayTime(music.getMusicFile().getSize(), 320000);
+
+        Music newMusic = new Music();
+        newMusic.setOriginalName(music.getName());
+        newMusic.setStoreName(storeName);
+        newMusic.setSinger(music.getSinger());
+        newMusic.setPlayTime(playTime);
+        newMusic.setCreateTime(music.getCreateTime());
+
+        return new IdDto(musicRepository.save(newMusic).getId());
+    }
+
+    @Transactional
+    public void deleteMusic(int id) {
+        musicRepository.deleteById(id);
+    }
 }
