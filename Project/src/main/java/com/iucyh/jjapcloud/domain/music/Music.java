a594e726236f38852b31514e41f12dcc5a523e84
Jaemin <ghdckdwn8456@gmail.com>
2025-06-04 22:35:15 +0900

    [chore] 엔티티에 더 자세한 정보 추가

--- Project/src/main/java/com/iucyh/jjapcloud/domain/music/Music.java
+++ Project/src/main/java/com/iucyh/jjapcloud/domain/music/Music.java
@@ -10,15 +10,29 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Entity
-@Table(name = "musics")
+@Table(
+        name = "musics",
+        indexes = {
+                @Index(name = "idx_original_name", columnList = "original_name"),
+                @Index(name = "idx_singer", columnList = "singer")
+        }
+)
 public class Music {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(length = 100, nullable = false)
     private String originalName;
+
+    @Column(length = 64)
     private String storeName;
+
+    @Column(length = 20, nullable = false)
     private String singer;
+
     private Long playTime;
+
     @CreationTimestamp
     private LocalDateTime createTime;
 
--- Project/src/main/java/com/iucyh/jjapcloud/domain/user/User.java
+++ Project/src/main/java/com/iucyh/jjapcloud/domain/user/User.java
@@ -7,13 +7,24 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "users")
+@Table(
+        name = "users",
+        indexes = {
+                @Index(name = "idx_email", columnList = "email")
+        }
+)
 public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(length = 64, unique = true, nullable = false)
     private String nickname;
+
+    @Column(length = 128, nullable = false)
     private String email;
+
+    @Column(length = 128, nullable = false)
     private String password;
 
     public User() {}
